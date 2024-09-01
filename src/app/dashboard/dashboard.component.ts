c7ad55d996fec50d52e2d63158559a197286e13c
Harrison Cooley <harrison_v_cooley@me.com>
2024-09-01 15:23:07 -0400

    Refactored subscribe to use new syntax with error msg

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,34 +16,28 @@ export class DashboardComponent implements OnInit{
 
   ngOnInit(): void {
     this.getTeams();
-    this.logger.info("The favorite teams list is: ", this.favoriteTeams)
-    this.logger.info("The favorite team names are: ", this.favoriteTeams.forEach(element => {
-      element.name 
-    }))
-    this.logger.info("The teams list is: ", this.teams)
-    this.logger.info("The team names are: ", this.teams.forEach(element => {
-      element.name 
-    }))
-    
   }
 
   getTeams(): void {
     this.teamService.getTeams()
-      .subscribe(teams => {
-        this.teams = teams;
-
-        // Populate favoriteTeams based on the teams array
-        this.favoriteTeams = teams.filter(team => team.isFavorite);
-
-        // Log the data after it's been fetched and processed
-        this.logger.info("The favorite teams list is: ", this.favoriteTeams);
-        this.logger.info("The favorite team names are: ", this.favoriteTeams.map(team => team.name));
-
-        this.logger.info("The teams list is: ", this.teams);
-        this.logger.info("The team names are: ", this.teams.map(team => team.name));
-        
-      }, error => {
-        this.logger.error('Error fetching teams:', error);
-      });
+      .subscribe({
+        next: teams => {
+          this.teams = teams;
+  
+          // Populate favoriteTeams based on the teams array
+          this.favoriteTeams = teams.filter(team => team.isFavorite);
+  
+          // Log the data after it's been fetched and processed
+          this.logger.info("The favorite teams list is: ", this.favoriteTeams);
+          this.logger.info("The favorite team names are: ", this.favoriteTeams.map(team => team.name));
+  
+          this.logger.info("The teams list is: ", this.teams);
+          this.logger.info("The team names are: ", this.teams.map(team => team.name));
+          
+        },
+        error: (e) => this.logger.error(e),
+        complete: () => this.logger.info("Completed loading data from teamService")
+      }
+      );
   }
 }
