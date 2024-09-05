d0b3e4c44c1dd14173b88c3a320f7ef0cd227679
Harrison Cooley <harrison_v_cooley@me.com>
2024-09-04 22:51:57 -0400

    Game detail page blueprint

---
+++ src/app/game-detail/game-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-detail.component.html',
+  styleUrl: './game-detail.component.css'
+})
+export class GameDetailComponent {
+
+}
